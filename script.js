9ea9f51ab608ba296c8dc183220854247e551957
BGDuyTruong <91254630+BGDuyTruong@users.noreply.github.com>
2022-10-23 11:32:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnstart = document.querySelector("#btn-start")
+const section = document.querySelector("section")
+
+const btnsubmit = document.querySelector("#submit-hidden")
+const section1 = document.querySelector("section")
+
+btnstart.addEventListener("click", () => {
+    section.style.display = "block"
+})
+
+btnstart.addEventListener("click", () => {
+    section1.style.display = "block"
+})
